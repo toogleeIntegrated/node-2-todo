5e1a97f266222ab58b049f5d4c3ecf8dfbbb4c91
Lucas Alcalde <Lucas Alcalde>
2017-04-25 10:53:54 -0500

    Added DELETE test cases

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.delete('/todos/:id', (req, res) => {
           return res.status(404).send();
         }
         // Happy
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
       res.status(400).send();
     });
